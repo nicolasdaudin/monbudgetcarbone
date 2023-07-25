08ce899d04ac708c033c770e6c85985f398b8706
nicdo77 <nicolas.daudin@gmail.com>
2023-07-25 08:59:31 +0200

    refactor: Refactor route computations in add travel usecase

--- src/application/usecases/add-flight-travel.usecase.ts
+++ src/application/usecases/add-flight-travel.usecase.ts
@@ -1,6 +1,6 @@
 import { convertDistance, getDistance, getPreciseDistance } from "geolib";
-import { FlightTravel } from "../../domain/flight-travel";
-import { AirportRepository } from "../airport.repository";
+import { FlightTravel, OutboundInboundType, Route } from "../../domain/flight-travel";
+import { Airport, AirportRepository } from "../airport.repository";
 import { DistanceCalculator } from "../distance-calculator";
 import { FlightTravelRepository } from "../flight-travel.repository";
 
@@ -27,152 +27,81 @@ export class AddFlightTravelUseCase {
     const fromAirport = await this.airportRepository.getByIataCode(addFlightTravelCommand.fromIataCode);
     const toAirport = await this.airportRepository.getByIataCode(addFlightTravelCommand.toIataCode);
 
+    let routes: Route[] = [];
 
-
-    if (addFlightTravelCommand.outboundConnection && !addFlightTravelCommand.inboundConnection) {
+    let outboundRoutes: Route[];
+    if (addFlightTravelCommand.outboundConnection) {
       const connectionAirport = await this.airportRepository.getByIataCode(addFlightTravelCommand.outboundConnection);
 
-      const distanceBeforeConnection = this.distanceCalculator.calculate(fromAirport.coordinates, connectionAirport.coordinates);
-      const kgCO2eqBeforeConnection = calculateFlightTravelCO2eqFromDistance(distanceBeforeConnection);
-
-      const distanceAfterConnection = this.distanceCalculator.calculate(connectionAirport.coordinates, toAirport.coordinates);
-      const kgCO2eqAfterConnection = calculateFlightTravelCO2eqFromDistance(distanceAfterConnection);
-
-      return await this.flightTravelRepository.add({
-        id: addFlightTravelCommand.id || 1,
-        user: addFlightTravelCommand.user,
-        routes: [{
-          type: 'outbound',
-          order: 1,
-          from: addFlightTravelCommand.fromIataCode,
-          to: addFlightTravelCommand.outboundConnection,
-          date: addFlightTravelCommand.outboundDate,
-          distance: distanceBeforeConnection,
-          kgCO2eq: kgCO2eqBeforeConnection,
-        },
-        {
-          type: 'outbound',
-          order: 2,
-          from: addFlightTravelCommand.outboundConnection,
-          to: addFlightTravelCommand.toIataCode,
-          date: addFlightTravelCommand.outboundDate,
-          distance: distanceAfterConnection,
-          kgCO2eq: kgCO2eqAfterConnection,
-        }]
-
-      })
-
+      outboundRoutes = this.computeRoutesWithConnection({ fromAirport, toAirport, connectionAirport, type: 'outbound', date: addFlightTravelCommand.outboundDate })
+    } else {
+      outboundRoutes = this.computeRouteWithoutConnection({ fromAirport, toAirport, type: 'outbound', date: addFlightTravelCommand.outboundDate });
     }
+    routes = routes.concat(outboundRoutes);
 
-    if (addFlightTravelCommand.outboundConnection && addFlightTravelCommand.inboundDate && addFlightTravelCommand.inboundConnection) {
-      const outboundConnectionAirport = await this.airportRepository.getByIataCode(addFlightTravelCommand.outboundConnection);
-
-      const distanceBeforeOutboundConnection = this.distanceCalculator.calculate(fromAirport.coordinates, outboundConnectionAirport.coordinates);
-      const kgCO2eqBeforeOutboundConnection = calculateFlightTravelCO2eqFromDistance(distanceBeforeOutboundConnection);
-
-      const distanceAfterOutboundConnection = this.distanceCalculator.calculate(outboundConnectionAirport.coordinates, toAirport.coordinates);
-      const kgCO2eqAfterOutboundConnection = calculateFlightTravelCO2eqFromDistance(distanceAfterOutboundConnection);
-
-      const inboundConnectionAirport = await this.airportRepository.getByIataCode(addFlightTravelCommand.inboundConnection);//?
-
-      const distanceBeforeInboundConnection = this.distanceCalculator.calculate(toAirport.coordinates, inboundConnectionAirport.coordinates);//?
-      const kgCO2eqBeforeInboundConnection = calculateFlightTravelCO2eqFromDistance(distanceBeforeInboundConnection);
-
-      const distanceAfterInboundConnection = this.distanceCalculator.calculate(inboundConnectionAirport.coordinates, fromAirport.coordinates);//?
-      const kgCO2eqAfterInboundConnection = calculateFlightTravelCO2eqFromDistance(distanceAfterInboundConnection);
-
-      return await this.flightTravelRepository.add({
-        id: addFlightTravelCommand.id || 1,
-        user: addFlightTravelCommand.user,
-        routes: [
-          {
-            type: 'outbound',
-            order: 1,
-            from: addFlightTravelCommand.fromIataCode,
-            to: addFlightTravelCommand.outboundConnection,
-            date: addFlightTravelCommand.outboundDate,
-            distance: distanceBeforeOutboundConnection,
-            kgCO2eq: kgCO2eqBeforeOutboundConnection,
-          },
-          {
-            type: 'outbound',
-            order: 2,
-            from: addFlightTravelCommand.outboundConnection,
-            to: addFlightTravelCommand.toIataCode,
-            date: addFlightTravelCommand.outboundDate,
-            distance: distanceAfterOutboundConnection,
-            kgCO2eq: kgCO2eqAfterOutboundConnection,
-          },
-          {
-            type: 'inbound',
-            order: 1,
-            from: addFlightTravelCommand.toIataCode,
-            to: addFlightTravelCommand.inboundConnection,
-            date: addFlightTravelCommand.inboundDate,
-            distance: distanceBeforeInboundConnection,
-            kgCO2eq: kgCO2eqBeforeInboundConnection,
-          },
-          {
-            type: 'inbound',
-            order: 2,
-            from: addFlightTravelCommand.inboundConnection,
-            to: addFlightTravelCommand.fromIataCode,
-            date: addFlightTravelCommand.inboundDate,
-            distance: distanceAfterInboundConnection,
-            kgCO2eq: kgCO2eqAfterInboundConnection,
-          }]
-
-      })
-
-    }
-
-    const distance = this.distanceCalculator.calculate(fromAirport.coordinates, toAirport.coordinates);
-    const kgCO2eq = calculateFlightTravelCO2eqFromDistance(distance);
 
 
     if (addFlightTravelCommand.inboundDate) {
-      return await this.flightTravelRepository.add({
-        id: addFlightTravelCommand.id || 1,
-        user: addFlightTravelCommand.user,
-        routes: [{
-          type: 'outbound',
-          from: addFlightTravelCommand.fromIataCode,
-          to: addFlightTravelCommand.toIataCode,
-          date: addFlightTravelCommand.outboundDate,
-          distance,
-          kgCO2eq,
-        },
-        {
-          type: 'inbound',
-          from: addFlightTravelCommand.toIataCode,
-          to: addFlightTravelCommand.fromIataCode,
-          date: addFlightTravelCommand.inboundDate,
-          distance,
-          kgCO2eq,
-        }]
+      let inboundRoutes: Route[];
+      if (addFlightTravelCommand.inboundConnection) {
+        const connectionAirport = await this.airportRepository.getByIataCode(addFlightTravelCommand.inboundConnection);
 
-      })
-    } else {
-      return await this.flightTravelRepository.add({
-        id: addFlightTravelCommand.id || 1,
-        user: addFlightTravelCommand.user,
-        routes: [{
-          type: 'outbound',
-          from: addFlightTravelCommand.fromIataCode,
-          to: addFlightTravelCommand.toIataCode,
-          date: addFlightTravelCommand.outboundDate,
-          distance,
-          kgCO2eq,
-        }]
-      })
+        inboundRoutes = this.computeRoutesWithConnection({ fromAirport: toAirport, toAirport: fromAirport, connectionAirport, type: 'inbound', date: addFlightTravelCommand.inboundDate })
+      } else {
+        inboundRoutes = this.computeRouteWithoutConnection({ fromAirport: toAirport, toAirport: fromAirport, type: 'inbound', date: addFlightTravelCommand.inboundDate });
+      }
+      routes = routes.concat(inboundRoutes);
     }
 
+    return await this.flightTravelRepository.add({
+      id: addFlightTravelCommand.id || 1,
+      user: addFlightTravelCommand.user,
+      routes
+    })
+  }
 
+  private computeRoutesWithConnection = ({ fromAirport, toAirport, connectionAirport, type, date }: { fromAirport: Airport, toAirport: Airport, connectionAirport: Airport, type: OutboundInboundType, date: Date }): Route[] => {
+    const distanceBeforeConnection = this.distanceCalculator.calculate(fromAirport.coordinates, connectionAirport.coordinates);
+    const kgCO2eqBeforeConnection = calculateFlightTravelCO2eqFromDistance(distanceBeforeConnection);
+
+    const distanceAfterConnection = this.distanceCalculator.calculate(connectionAirport.coordinates, toAirport.coordinates);
+    const kgCO2eqAfterConnection = calculateFlightTravelCO2eqFromDistance(distanceAfterConnection);
+    return [{
+      type,
+      order: 1,
+      from: fromAirport.iataCode,
+      to: connectionAirport.iataCode,
+      date,
+      distance: distanceBeforeConnection,
+      kgCO2eq: kgCO2eqBeforeConnection,
+    },
+    {
+      type,
+      order: 2,
+      from: connectionAirport.iataCode,
+      to: toAirport.iataCode,
+      date,
+      distance: distanceAfterConnection,
+      kgCO2eq: kgCO2eqAfterConnection,
+    }]
 
   }
 
-
+  private computeRouteWithoutConnection = ({ fromAirport, toAirport, type, date }: { fromAirport: Airport, toAirport: Airport, type: OutboundInboundType, date: Date }): Route[] => {
+    const distance = this.distanceCalculator.calculate(fromAirport.coordinates, toAirport.coordinates);
+    const kgCO2eq = calculateFlightTravelCO2eqFromDistance(distance);
+    return [{
+      type,
+      from: fromAirport.iataCode,
+      to: toAirport.iataCode,
+      date,
+      distance,
+      kgCO2eq,
+    }]
+  }
 }
+
+
 
 
 
